7f5eab0e8e8b8cea0b98835b5ad95d390e67cafe
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-26 17:42:10 +0600

    feat: implement counter functionality with Redux integration

--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -1,5 +1,3 @@
-// create slice for counter
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -9,7 +7,18 @@ const initialState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    increment: (state, payload) => {
+      state.count += payload.payload || 1;
+    },
+    decrement: (state, payload) => {
+      state.count -= payload.payload || 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
 });
 
+export const { increment, decrement, reset } = counterSlice.actions;
 export const counterReducer = counterSlice.reducer;
