8ceba614766ed4930df318335221a40144cad769
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-26 17:44:46 +0600

    fix: correct parameter naming in increment and decrement reducers

--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -8,11 +8,11 @@ const counterSlice = createSlice({
   name: "counter",
   initialState: initialState,
   reducers: {
-    increment: (state, payload) => {
-      state.count += payload.payload || 1;
+    increment: (state, action) => {
+      state.count += action.payload || 1;
     },
-    decrement: (state, payload) => {
-      state.count -= payload.payload || 1;
+    decrement: (state, { payload }) => {
+      state.count -= payload || 1;
     },
     reset: (state) => {
       state.count = 0;
