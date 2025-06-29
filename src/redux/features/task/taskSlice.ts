6a05c8fee36a1777ab6a4eae74c3f0c2d8387790
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-29 23:26:33 +0600

    feat: reintroduce taskSlice with initial state and addTask reducer for task management

--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -1,8 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  task: {
+    id: "1",
+    title: "Title of the task",
+    description: "Description of the task",
+    status: "pending", // or "completed"
+    dueDate: "2023-10-01",
+    completed: false,
+    priority: "high", // or "medium", "low"
+  },
+};
+
 const taskSlice = createSlice({
   name: "task",
-  initialState: {},
+  initialState,
   reducers: {
     addTask: (state, action) => {
       console.log(state, action);
