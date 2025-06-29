086e4808fbdb3eb8a07dab4406293e8df0cac807
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-29 16:38:28 +0600

    feat: add new UI components and enhance styling with Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <h1>Counter app</h1>;
+  return (
+    <div>
+      Counter app
+      <Button>Increment</Button>
+    </div>
+  );
 }
