81c44a0c279d3d9d037d5f72ae2754e959789032
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-07-01 00:10:12 +0600

    feat: add baseApi for task queries and integrate into store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./features/counter/counterSlice";
 import { taskReducer } from "./features/task/taskSlice";
 import { userReducer } from "./features/user/userSlice";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     tasks: taskReducer,
     users: userReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
 });
 
